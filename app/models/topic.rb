ee69446539350998c2ddfe67882d8c19d09b90a8
SUGIMURA JUN <sugimurajun@SUGIMURAnoMacBook-Air.local>
2018-03-23 20:47:43 +0900

    initial commit

---
+++ app/models/topic.rb
@@ -0,0 +1,21 @@
+class Topic < ApplicationRecord
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+ 
+  belongs_to :user
+
+  has_many :topics
+
+  has_many :favorites
+
+  has_many :favorite_users, through: :favorites, source: 'user'
+
+  mount_uploader :image, ImageUploader
+
+  has_secure_password
+
+
+end
+
+
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  get 'favorites/index'
+
+  get 'topics/new'
+
+  get 'sessions/new'
+
+  root 'pages#index'
+
+  get 'pages/help'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources 'users'
+  resources 'topics'
+  post '/favorites', to:'favorites#create'
+  get '/favorites', to: 'favorites#index'
+
+end
+
